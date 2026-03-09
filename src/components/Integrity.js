7d0b6ff78b921e4637da4962786a561e993d5823
Yahya Saad <112628478+YahyaSaad1@users.noreply.github.com>
2026-03-09 15:50:20 +0200

    Update Integrity.js

--- src/components/Integrity.js
+++ src/components/Integrity.js
@@ -6,19 +6,32 @@ import ComAbout from './ComAbout'
 
 export default function Integrity(){
     return(
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center py-16 px-6 md:px-12 lg:px-16 xl:px-20 gap-x-40">
-            <div className='grid grid-cols-2 gap-4 items-start'>
-                <div className='flex flex-col gap-y-4 items-center'>
-                    <img className='rounded-3xl w-60' src={image1} alt="image1" />
-                    <img className='rounded-3xl w-60' src={image2} alt="image2" />
+       <div className="grid grid-cols-1 lg:grid-cols-2 items-center py-16 px-6 md:px-12 lg:px-20 gap-12 lg:gap-20 bg-white">
+            
+            {/* حاوية الصور */}
+            <div className='grid grid-cols-2 gap-4 items-start order-2 lg:order-1'>
+                <div className='flex flex-col gap-y-4'>
+                    <img className='rounded-3xl w-full shadow-lg' src={image1} alt="Medical professional 1" />
+                    <img className='rounded-3xl w-full shadow-lg' src={image2} alt="Medical professional 2" />
                 </div>
 
-                <div className='flex flex-col gap-y-4 items-center mt-12'>
-                    <img className='rounded-3xl w-60' src={image3} alt="image3" />
-                    <img className='rounded-3xl w-60' src={image4} alt="image4" />
+                {/* تأثير الإزاحة (Offset) */}
+                <div className='flex flex-col gap-y-4 mt-10 md:mt-16'>
+                    <img className='rounded-3xl w-full shadow-lg' src={image3} alt="Medical professional 3" />
+                    <img className='rounded-3xl w-full shadow-lg' src={image4} alt="Medical professional 4" />
                 </div>
             </div>
-            <ComAbout hint='Integrity' to='/#' title='Our Stellar Values' desc='The cornerstone of our establishment is ‘Making the benefits of exceptional medical services reach the people without Discrimination.’ We strive to live up to this philosophy through our stellar values, that are the pillars of every service that we offer under the banner of our prestigious hospital.' />
+
+            {/* نصوص القسم */}
+            <div className="order-1 lg:order-2">
+                <ComAbout 
+                    hint='Integrity' 
+                    to='/#' 
+                    title='Our Stellar Values' 
+                    desc='The cornerstone of our establishment is ‘Making the benefits of exceptional medical services reach the people without Discrimination.’ We strive to live up to this philosophy through our stellar values, that are the pillars of every service that we offer under the banner of our prestigious hospital.' 
+                />
+            </div>
         </div>
     )
+
 }
