f64e0d721687ed9c59b51339371e04f3699f7656
Yahya Saad <112628478+YahyaSaad1@users.noreply.github.com>
2026-03-09 15:52:43 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar(){
     return(
         <div className="flex items-center justify-between py-4 px-6 md:px-12 lg:px-16 xl:px-20 shadow-xl bg-first-color">
             <div className="flex tracking-wider gap-1 md:gap-2 whitespace-nowrap">
-                <h2 className="font-bold text-xl lg:text-3xl scale-y-105 text-text-color">Muhiris</h2>
-                <h2 className="font-bold text-xl lg:text-3xl scale-y-105 text-black">Doctor</h2>
+                <h2 className="font-bold text-2xl lg:text-3xl scale-y-105 text-text-color">Muhiris</h2>
+                <h2 className="font-bold text-2xl lg:text-3xl scale-y-105 text-black">Doctor</h2>
             </div>
             <div className="hidden md:flex">
                 <NavLink/>
@@ -18,3 +18,4 @@ export default function Navbar(){
     )
 
 }
+
